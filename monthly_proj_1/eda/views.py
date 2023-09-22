31a7e2324b5b39fff9d920f1550d7ea842368bbf
radiant <ehdwn1tp@naver.com>
2023-09-22 12:42:15 +0900

    피드백 일부 반영_2

--- monthly_proj_1/eda/views.py
+++ monthly_proj_1/eda/views.py
@@ -4,6 +4,6 @@ import os
 # Create your views here.
 def homepage(req):
     img_src = os.listdir('assets/images')
-    hypos = [list(filter(lambda x: f'theory_{i+1}' in x, img_src)) for i in range(len(img_src))]
-    variables = {f'hypo{i+1}':hypos[i] for i in range(len(hypos))}
+    hypos = [list(filter(lambda x: f'theory_{i}' in x, img_src)) for i in range(len(img_src))]
+    variables = {f'hypo{i}':hypos[i] for i in range(len(hypos)) if len(hypos[i]) != 0}
     return render(req, 'home.html', variables)
